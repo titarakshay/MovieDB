58a5a636c3442bd5ec34495cbbfc37f82e70bf0a
titarakshay <titarakshay@gmail.com>
2022-02-12 00:05:38 +0530

    add packages

---
+++ src/Components/Header.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+// import Search from "./Search";
+
+export default function Header() {
+  return (
+    <header className="header ">
+      <div className="container-xl text-white">
+        <nav>
+          {/* <NavLink to="/" style={{ textDecoration: "none", color: "black" }}>
+          </NavLink> */}
+          <h1>MovieDB</h1>
+        </nav>
+      </div>
+    </header>
+  );
+}
